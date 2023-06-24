447c1b915402eb3121f900d0fb2e3e990a9fe52e
jasm1n2001 <khujovahafiza@gmail.com>
2023-06-24 15:24:03 +0300

    first commit

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿ int k = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+  for(int i = n; i <= k; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            System.Console.WriteLine($"{i} * {j} = {i*j}");
+        }
+    System.Console.WriteLine();
+    }
+
+
---
+++ task4/User.cs
@@ -0,0 +1,20 @@
+namespace task4;
+
+public class User
+{
+    public string FirstName;
+    public string LastName;
+    public string UserName;
+    public string Password;
+
+List<User> Arry = new List<User>
+
+    public string GetInfo()
+    {
+        System.Console.WriteLine($"{FirstName} {LastName} ");
+    }
+    public string Login()
+    {
+
+    }
+}
